092c0467bf00808e3ec3877878a1b4ee38d255e2
mbeverly18 <64284566+mbeverly18@users.noreply.github.com>
2020-10-13 19:42:01 -0500

    finished function

---
+++ main.js
@@ -0,0 +1,8 @@
+function tellFortune(children, partner, location, job) {
+    // code
+    console.log("You will be " + job + " in, " + location + " and married to " + partner + " with " + children + " kids ")
+}
+
+tellFortune(3, "Marquez", "Clarksville", "Technician");
+tellFortune(2, "LeBron", "Chicago", "Veterinarian");
+tellFortune(1, "Game", "Los Angeles", "Web Developer");
